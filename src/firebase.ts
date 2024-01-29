5fb491ee85045249d6562cabc5ea9929d12da0fe
msahn <anms1024@cwcoms.co.kr>
2024-01-29 10:58:33 +0900

    initial commit

---
+++ src/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBQ4AMqfv-OiYA9aQtVWZ8hOj7PG2DfZpw",
+  authDomain: "nwitter-reloaded-f1cce.firebaseapp.com",
+  projectId: "nwitter-reloaded-f1cce",
+  storageBucket: "nwitter-reloaded-f1cce.appspot.com",
+  messagingSenderId: "754370755651",
+  appId: "1:754370755651:web:9974c00b0ae41c060d68f6",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
---
+++ src/routes/login.tsx
@@ -0,0 +1,83 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { FirebaseError } from "firebase/app";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
+import {
+  Wrapper,
+  Form,
+  Title,
+  Input,
+  Switcher,
+  Error,
+} from "../components/auth-components";
+import GithubButton from "../components/github-btn";
+
+export default function CreateAccount() {
+  const navigate = useNavigate();
+  const [isLoading, setLoading] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { name, value },
+    } = e;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setError("");
+
+    if (isLoading || !email || !password) return;
+
+    try {
+      setLoading(true);
+      await signInWithEmailAndPassword(auth, email, password);
+
+      navigate("/");
+    } catch (e) {
+      if (e instanceof FirebaseError) {
+        setError(e.message);
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+  return (
+    <Wrapper>
+      <Title>Log Into X</Title>
+      <Form onSubmit={onSubmit}>
+        <Input
+          name="email"
+          value={email}
+          placeholder="Email"
+          type="email"
+          required
+          onChange={onChange}
+        />
+        <Input
+          name="password"
+          value={password}
+          placeholder="Password"
+          type="password"
+          required
+          onChange={onChange}
+        />
+        <Input type="submit" value={isLoading ? "Loading..." : "Log in"} />
+      </Form>
+      {error ? <Error>{error}</Error> : null}
+      <Switcher>
+        Don't have an account?{" "}
+        <Link to="/create-account">Create one &rarr;</Link>
+      </Switcher>
+      <GithubButton />
+    </Wrapper>
+  );
+}
